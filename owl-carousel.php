5981222aec92747e75783e5fcec33c3380acfcae
TStrothjohann <thomas@strothjohann.net>
2016-10-30 22:03:47 +0100

    Changed plugin name to solve update confusion in wp backend

--- owl-carousel.php
+++ owl-carousel.php
@@ -1,10 +1,10 @@
 <?php
 /*
-Plugin Name: Owl Carousel
+Plugin Name: mmbeta Owl Carousel
 Plugin URI: http://owlgraphic.com/owlcarousel/
 Description: Wordpress Plugin integrating Owl Carousel
 Author: Thomas Strothjohann
-Version: 0.5
+Version: 1.0
 Author URI: http://codereporter.de
 */
 
@@ -19,6 +19,6 @@ if ( ! defined( 'WPINC' ) ) {
 function owl_scripts() {
   wp_enqueue_style( 'owl-carousel', plugins_url( 'owl.carousel.css', __FILE__ ) );
   wp_enqueue_style( 'owl-carousel-theme', plugins_url( 'owl.theme.css', __FILE__ ) );
-  wp_enqueue_script( 'owl-carousel-js', plugins_url( 'owl.carousel.min.js', __FILE__ ) , array( 'jquery' ), '20160210', true );
+  wp_enqueue_script( 'owl-carousel-js', plugins_url( 'owl.carousel.min.js', __FILE__ ) , array( 'jquery' ), '20161030', true );
 }
 add_action( 'wp_enqueue_scripts', 'owl_scripts' );
